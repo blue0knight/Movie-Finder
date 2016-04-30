72b606cedb7afbf2556bd9f731489f9cd7ef3e02
blue0knight <oni_rivera98@yahoo.com>
2016-04-29 23:29:22 -0400

    Add Siskel to fetch movie data

--- server.rb
+++ server.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
+require './models/siskel.rb'
 
 get '/' do
   erb :index
   # "MovieFinder"
+end
+
+get '/movie' do
+  movie = Siskel.new("Lion King")
+  "This movie title is #{movie.title}"
 end
