f2cc946b28f9893ad09e364c7fce28636b283caa
blue0knight <oni_rivera98@yahoo.com>
2016-05-09 18:17:32 -0400

    adding missing files

---
+++ movie_finder_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require './'
+
+class TestMovieFinder < Minitest::Test
+
+  def setup
+    @search = Search.new
+  end
+
+  
+end
--- server.rb
+++ server.rb
@@ -2,11 +2,15 @@ require 'sinatra'
 require './models/siskel.rb'
 
 get '/' do
+  title = params[:title]
+  @movie = Siskel.new(title)
   erb :index
+  # params.to_s
+  # erb :index
   # "MovieFinder"
 end
 
 get '/movie' do
-  movie = Siskel.new("Lion King")
-  "This movie title is #{movie.title}"
+  @movie = Siskel.new("Lion King")
+  erb :index
 end
